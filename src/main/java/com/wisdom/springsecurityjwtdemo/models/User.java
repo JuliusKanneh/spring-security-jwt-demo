81092302e3a4cff329de25b62a3727a9828c94fe
JuliusKanneh <juliuskanneh7@gmail.com>
2022-04-18 11:45:29 +0200

    Making the userDetails dynamic from mysql datasource

---
+++ src/main/java/com/wisdom/springsecurityjwtdemo/models/User.java
@@ -0,0 +1,69 @@
+package com.wisdom.springsecurityjwtdemo.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String username;
+    private String password;
+    private boolean active;
+    private String roles;
+
+    public User() {
+    }
+
+    public User(long id, String username, String password, boolean active, String roles) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.active = active;
+        this.roles = roles;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public String getRoles() {
+        return roles;
+    }
+
+    public void setRoles(String roles) {
+        this.roles = roles;
+    }
+}
--- src/main/java/com/wisdom/springsecurityjwtdemo/services/MyUserDetailsService.java
+++ src/main/java/com/wisdom/springsecurityjwtdemo/services/MyUserDetailsService.java
@@ -1,18 +1,31 @@
 package com.wisdom.springsecurityjwtdemo.services;
 
-import org.springframework.security.core.userdetails.User;
+import com.wisdom.springsecurityjwtdemo.models.MyUserDetails;
+import com.wisdom.springsecurityjwtdemo.models.User;
+import com.wisdom.springsecurityjwtdemo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.nio.file.attribute.UserPrincipal;
 import java.util.ArrayList;
+import java.util.Optional;
 
 @Service
 public class MyUserDetailsService implements UserDetailsService {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return new User("user", "pass", new ArrayList<>());
+//        return new User("user", "pass", new ArrayList<>());
+        Optional<User> user = userRepository.findByUsername(username);
+
+        user.orElseThrow(()-> new UsernameNotFoundException("Username not found"));
+
+        return user.map(MyUserDetails::new).get();
     }
 }
